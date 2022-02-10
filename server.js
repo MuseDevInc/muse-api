7e599582905d99e12eb334413d6415a11382c1c5
cdizon239 <coleencharmille@gmail.com>
2022-02-09 22:34:56 -0500

    spotify setup initial

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
 const app = express()
 app.set('port', process.env.PORT || 4000)
 const SESSION_SECRET = process.env.SESSION_SECRET
-// const bodyParser = require("body-parser")
-// app.use(bodyParser.urlencoded({ extended: true }))
+const cors = require('cors');
 // const methodOverride = require('method-override')
-// app.use(methodOverride('_method'))
 const session = require('express-session')
 const museController = require('./controllers/muse')
 const sessionController = require('./controllers/session')
+const spotifyPingController = require('./controllers/spotifyPing')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride('_method'))
+
 //GitPlz
 //MIDDLEWARE
 
@@ -19,7 +24,6 @@ app.use(session({
     saveUninitialized: false,
 }))
 // app.use(express.static('public'))
-
 
 //Make username available on all pages
 app.use((req,res,next) => {
@@ -46,10 +50,9 @@ const authRequired = (req,res,next) => {
     }
 }
 
-
 app.use('/muse', museController)
 app.use('/session', sessionController)
-
+app.use('/spot', spotifyPingController)
 
 app.listen(app.get('port') , () => {
     console.log(`Working on port: ${app.get('port')}`)
