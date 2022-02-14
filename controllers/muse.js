f7d69a01585a088bcab7db7b8f8157c395c15350
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-13 18:51:21 -0700

    actually return all unswiped profiles to app for userQueue

--- controllers/muse.js
+++ controllers/muse.js
@@ -5,43 +5,62 @@ const User = require("../models/user");
 
 //Fetch for discover
 //Show individual user page, (Maybe not Profile?)
-router.get('/discover', (req,res) => {
-    Profile.find({}, (error,profiles) => {
-    if (error) {
-        res.status(400).json({ error: error.message })
-      }
-      console.log(profiles)
-      res.status(200).json(profiles)
-}) 
-})
+
+router.get("/discover/:id", (req, res) => {
+  console.log("discover hit");
+  //return current user profile and return ids in swipedleft and swipedright arrays
+  Profile.findOne(
+    { owner: req.session.userId },
+    "swipedLeft swipedRight",
+    (error, swipedIds) => {
+        //callback executes and concatenates swipedLeft and SwipeRight
+      let swipedIdsArray = swipedIds.swipedLeft.concat(swipedIds.swipedRight);
+      //find all docs where no value in swipedIdsArray is present in id field
+      Profile.find(
+        { id: { $not: { $all: swipedIdsArray } } },
+        (error, unswiped) => {
+          console.log(swipedIdsArray);
+          if (error) {
+            res.status(400).json({ error: error.message });
+          }
+          //return all profile docs to app, userQueue will only include profile objects user has not swiped on previously.
+          console.log(unswiped + " have not been swiped");
+          return res.status(200).json(unswiped);
+        }
+      );
+    }
+  );
+});
+//patch currentUser with swipe
 
 // USER PAGE
-router.get('/userPage', (req,res) => {
-    console.log('Hello I got hit', req.session.userId)
-    Profile.findOne({owner: req.session.userId}, (error, profile) => {
-        if (error) {
-            res.status(400).json({ error: error.message })
-            next()
-        }
-        //return profile as json
-      return res.status(200).json(profile)
-    })
-    // User.findById(req.session.userId, (error, user) => {
-    //     if (error) {
-    //         res.status(400).json({ error: error.message })
-    //         next()
-    //     }
-    //     Profile.find({owner: req.session.userId}, (error, profile) => {
-    //         if (error) {
-    //             res.status(400).json({ error: error.message })
-    //             next()
-    //         }
-    //         //return profile as json
-    //       return res.status(200).json(profile)
+router.get("/userPage", (req, res) => {
+  console.log("Hello I got hit", req.session.userId);
+  Profile.findOne({ owner: req.session.userId }, (error, profile) => {
+    console.log(error, profile);
+    if (error) {
+      res.status(400).json({ error: error.message });
+      next();
+    }
+    //return profile as json
+    return res.status(200).json(profile);
+  });
+  // User.findById(req.session.userId, (error, user) => {
+  //     if (error) {
+  //         res.status(400).json({ error: error.message })
+  //         next()
+  //     }
+  //     Profile.find({owner: req.session.userId}, (error, profile) => {
+  //         if (error) {
+  //             res.status(400).json({ error: error.message })
+  //             next()
+  //         }
+  //         //return profile as json
+  //       return res.status(200).json(profile)
 
-    //     })
-    // })
-})
+  //     })
+  // })
+});
 // Page where User who has signed up creates their profile. This is not a register page.
 router.post("/userCreationPage", (req, res) => {
   console.log(req.session);
