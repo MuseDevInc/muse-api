5dc3e93587d07dfdeb0c531ee0c6c622ab4118ed
cdizon239 <coleencharmille@gmail.com>
2022-02-21 15:27:39 -0500

    profile owner change

--- controllers/muse.js
+++ controllers/muse.js
@@ -94,7 +94,7 @@ router.post("/discover/checkmatch", (req, res) => {
 // USER PAGE
 router.get("/userPage", (req, res) => {
   console.log("Hello I got hit", req.session.userId);
-  Profile.findOne({ owner: ObjectId(req.session.userId)}, (error, profile) => {
+  Profile.findOne({ owner: req.session.userId}, (error, profile) => {
     console.log(error, profile);
     if (error) {
       res.status(400).json({ error: error.message });
@@ -108,7 +108,7 @@ router.get("/userPage", (req, res) => {
 
 router.get("/getUsers/:id", (req, res) => {
 
-  Profile.findOne({ owner: ObjectId(req.session.userId) })
+  Profile.findOne({ owner: req.params.id })
       .populate("owner")
       .then(userProfile=> {
           return res.status(200).json(userProfile)})
